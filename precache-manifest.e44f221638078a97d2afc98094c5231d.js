7974887d95c57e3425210f54e95549a9396bd43d
tovoniaina <idealy371@gmail.com>
2022-03-03 11:08:35 +0000

    Deploying to gh-pages from @ tovoniaina/Madasafaritours@772074bb6570a41cd6ba980d51a1c1ecbb332396 🚀

---
+++ precache-manifest.e44f221638078a97d2afc98094c5231d.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e57ca861256f9019dcd1fe725afb689a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64347119739c7af56c3f",
+    "url": "/static/css/2.b458499b.chunk.css"
+  },
+  {
+    "revision": "519f807a97d826467cac",
+    "url": "/static/css/main.4316b301.chunk.css"
+  },
+  {
+    "revision": "64347119739c7af56c3f",
+    "url": "/static/js/2.dde2e307.chunk.js"
+  },
+  {
+    "revision": "df43f54944fec9b1e47c37ca5cf32ad9",
+    "url": "/static/js/2.dde2e307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c545af9e848b6c606ff",
+    "url": "/static/js/3.47208d72.chunk.js"
+  },
+  {
+    "revision": "519f807a97d826467cac",
+    "url": "/static/js/main.d58505a0.chunk.js"
+  },
+  {
+    "revision": "730bb82542518a8a1972",
+    "url": "/static/js/runtime-main.db75b275.js"
+  },
+  {
+    "revision": "851874c9c770f78da29b5a3b8f7b34c1",
+    "url": "/static/media/BaobabSafari.851874c9.jpg"
+  },
+  {
+    "revision": "fa4e91629b2442ba3a8c696384852afc",
+    "url": "/static/media/Caroussel1.fa4e9162.jpg"
+  },
+  {
+    "revision": "d358afe0641dc171dcdbade1f0fd907a",
+    "url": "/static/media/Caroussel2.d358afe0.jpg"
+  },
+  {
+    "revision": "a4132272039aef6e523c4d35cacd6918",
+    "url": "/static/media/Caroussel3.a4132272.jpg"
+  },
+  {
+    "revision": "4ae232f2b9de40fb1ce74259b3af28b8",
+    "url": "/static/media/DivingTour.4ae232f2.jpg"
+  },
+  {
+    "revision": "346ec33d373477c502db351d632ee4f8",
+    "url": "/static/media/DryDeciduousBirding.346ec33d.jpg"
+  },
+  {
+    "revision": "2676dc381496f7e4e1dee71d3628a997",
+    "url": "/static/media/EasternWonder.2676dc38.png"
+  },
+  {
+    "revision": "1b3d06501b1d83514c4ceea96af3b18c",
+    "url": "/static/media/MadaNorthernSafari.1b3d0650.jpg"
+  },
+  {
+    "revision": "27e84e264058d393c50debf855090583",
+    "url": "/static/media/MadaSouthernSafari.27e84e26.jpg"
+  },
+  {
+    "revision": "6be9abfb2e58c8e3a1134b100a07611d",
+    "url": "/static/media/MalagasyCultureDiscovery.6be9abfb.jpg"
+  },
+  {
+    "revision": "66e1d6eb9393fb5bdb55d50474b73ce0",
+    "url": "/static/media/MarvellousHoneymoonSafari.66e1d6eb.jpg"
+  },
+  {
+    "revision": "c9f1bd6e42cfa9f36e5a6b3dacec2a1c",
+    "url": "/static/media/RainForestBirding.c9f1bd6e.jpg"
+  },
+  {
+    "revision": "a2c830b7de3ceb0d4c1726cf48d1ef19",
+    "url": "/static/media/RiverTrip.a2c830b7.jpg"
+  },
+  {
+    "revision": "19a0ec6b42eb1ba06f96cc0ae74b7a6a",
+    "url": "/static/media/SouthernTrekking.19a0ec6b.jpg"
+  },
+  {
+    "revision": "f80b7186a7ea84e425104afebeaec89e",
+    "url": "/static/media/TanaSurrounding.f80b7186.jpg"
+  },
+  {
+    "revision": "e7bffd2af38620e53eab677d7ec47307",
+    "url": "/static/media/VanillaCruise.e7bffd2a.jpg"
+  },
+  {
+    "revision": "08cf9af410268e84de38894e814a38bb",
+    "url": "/static/media/aristide-5.08cf9af4.png"
+  },
+  {
+    "revision": "937fdb15ca6a6fbe27457f88456b4326",
+    "url": "/static/media/indri.937fdb15.JPG"
+  },
+  {
+    "revision": "d5ab0166a0a35edca29cf67898a8e7f3",
+    "url": "/static/media/logo.d5ab0166.png"
+  },
+  {
+    "revision": "482fb40339ec04a3c1a74bbcd6567027",
+    "url": "/static/media/marojejy.482fb403.png"
+  },
+  {
+    "revision": "f517848ddae04bc942c80745939019c6",
+    "url": "/static/media/team-3.f517848d.jpg"
+  },
+  {
+    "revision": "8525c30ba089bb4fdd45152ca6d190ad",
+    "url": "/static/media/team-4.8525c30b.png"
+  }
+]);
